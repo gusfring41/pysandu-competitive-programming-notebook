42867eae1cb91cd5c0917c4708df1d9d8d9c4e1f
gusfring41 <gusfring.a@gmail.com>
2026-05-04 17:48:48 -0300

    template and compilation flags

---
+++ code/template/template.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std; 
+
+//#define int long long 
+#define vi vector<int>
+#define pii pair<int, int>
+#define debug(x...) cout<<"["#x"]: ",[](auto...$){((cout<<$<<"; "),...);}(x),cout<<endl
+
+void solve(){
+
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    //cin >> t;
+    while(t--) solve();
+    return 0;
+}
