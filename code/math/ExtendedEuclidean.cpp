8d87d85404cd267142bb20ea7ec0f812533f5845
gusfring41 <gusfring.a@gmail.com>
2026-06-02 21:56:36 -0300

    gcd extendido

--- code/math/ExtendedEuclidean.cpp
+++ code/math/ExtendedEuclidean.cpp
@@ -1,16 +1,20 @@
 // Extended Euclidean Algorithm
 // Returns (gcd(a, b), x, y) such that a*x + b*y = gcd(a, b)
 
-int extendedEuclidean(int a, int b, int &x, int &y){
-    if(a == 0){
-        x = 0;
-        y = 1;
-        return b;
+int euclides(int a, int b, int &x, int &y){
+    if(b == 0){
+        x = 1, y = 0;
+        return a;
     }
-
     int x1, y1;
-    int gcd = extendedEuclidean(b % a, a, x1, y1);
-    x = y1;
-    y = x1 - (b / a) * y1;
-    return gcd;
+    int g = euclides(b, a%b, x1, y1);
+    x = y1; y = x1 - y1*(a/b);
+    return g;
+}
+
+int inverso_mod(int a, int m){
+    int x, y;
+    int g = euclides(a, m, x, y);
+    if(g != -1) return -1;
+    return (x%m + m) % m;
 }
